3e90acae41177fea9b016d8e5051de6d1cfbbf16
greglindahl <greglindahl410@gmail.com>
2015-10-14 16:46:48 -0600

    More questions

--- grocery.js
+++ grocery.js
@@ -2,8 +2,6 @@
 
 $(document).ready(function(){
 
-
-
   var groceries = [
     {name: "Tomatoes", status: "needed", price: "3.99", quantity: 5},
     {name: "Onions", status: "needed", price: "1.85", quantity: 2},
@@ -11,14 +9,30 @@ $(document).ready(function(){
     {name: "Limes", status: "complete", price: ".33", quantity: 3},
     {name: "Jalapeno", status: "complete", price: ".15", quantity: 2}
   ];
+
 //1. Add groceries to <ul>. Store status, price and quantity as data attributes
+for(i = 0; i < groceries.length; i++) {
+  $("#list").append("<li class='listItem' data-status='" + groceries[i].status + "' data-price='" + groceries[i].price + "' data-quantity='" + groceries[i].quantity + "'><h2>" + groceries[i].name + "</h2></li>");
+}
 
 //2. Give 'needed' items a background-color of your choosing. Do the same for 'complete' items.
 //   Choose any text color you desire.
+$( "li[data-status*='needed']" ).css('background-color', '#FFB00F');
+$('li[data-status*="complete"]').css('background-color', 'green');
 
 //3. Display the item quantity next to it's name. (  i.e. Tomatoes (5)  )
+for(i = 0; i < groceries.length; i++) {
+  $("#list").append("'<p>' + .listItem.quantity + ' ' + '</p>'");
+}
+// $("'<p>' + .listItem.quantity + ' ' + '</p>'").appendTo(".listItem");
 
 //4. Display total quantity and cost on the page.
+function totalCost(cost) {
+	for (i = 0; i < groceries[i].price.length; i++) {
+		var groceriesPrice = parseFloat(groceries[0].price) + parseFloat(groceries[1].price) + parseFloat(groceries[2].price) + parseFloat(groceries[3].price) + parseFloat(groceries[4].price);
+		var groceriesCost = groceriesPrice.toFixed(2);
+	}
+}
 
 //5. When an item is hovered over, it's background should darken slightly. This can be done through CSS but use
 //   JS for practice.
